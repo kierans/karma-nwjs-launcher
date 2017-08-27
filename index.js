36b68d8d534c498f0c23a18cf6b1197e38e196d6
Ben Schmidt <insightfuls@users.noreply.github.com>
2017-08-28 09:29:32 +1000

    Fix the algorithm to work asynchronously.

--- index.js
+++ index.js
@@ -6,28 +6,7 @@ var merge = require('merge');
 var copy = require('copy');
 var builder = require('nwjs-builder-phoenix');
 
-function findpath() {
-  /*
-   * Assuming this script is at <project_root>/node_modules/karma-nwjs-phoenix-launcher,
-   * this should resolve to <project_root>/package.json.
-   */
-  var pkgPath = path.join(path.resolve(__dirname, "../.."), "package.json");
-  var pkg = JSON.parse(fs.readFileSync(pkgPath));
-  var nwVersion = pkg.build.nwVersion;
-
-  var downloadedNwjs = (new builder.Downloader({
-    platform: process.platform,
-    arch: process.arch,
-    version: nwVersion,
-    flavor: 'sdk',
-    useCaches: true,
-    showProgress: true
-  })).fetchAndExtract();
-
-  return builder.findExecutable(process.platform, downloadedNwjs);
-}
-
-var NodeWebkitBrowser = function(baseBrowserDecorator, args,config) {
+var NodeWebkitBrowser = function(baseBrowserDecorator, args, config) {
   baseBrowserDecorator(this);
 
   var customOptions = args.options || {};
@@ -35,6 +14,45 @@ var NodeWebkitBrowser = function(baseBrowserDecorator, args,config) {
     return path.join(process.cwd(), searchPath);
   });
   searchPaths.unshift(process.env.NODE_PATH);
+
+  this._findCommand = function(callback) {
+    var command = this._getCommand();
+
+    if (command !== null) {
+      callback(null, command);
+
+      return;
+    }
+
+    /*
+     * Assuming this script is at <project_root>/node_modules/karma-nwjs-phoenix-launcher,
+     * this should resolve to <project_root>/package.json.
+     *
+     * It won't work if the package is installed via `npm link` because in that scenario
+     * `__dirname` provides the module's original path, not its installed path.
+     */
+    var pkgPath = path.join(path.resolve(__dirname, "../.."), "package.json");
+    var pkg = JSON.parse(fs.readFileSync(pkgPath));
+    var nwVersion = pkg.build.nwVersion;
+  
+    (new builder.Downloader({
+      platform: process.platform,
+      arch: process.arch,
+      version: nwVersion,
+      flavor: 'sdk',
+      useCaches: true,
+      showProgress: true
+    }))
+    .fetchAndExtract()
+    .then(function(extracted) {
+      return builder.findExecutable(process.platform, extracted);
+    })
+    .then(function(found) {
+      callback(null, found);
+    }, function(err) {
+      callback(err);
+    });
+  };
 
   // used by Karma
   this._start = function(url) {
@@ -45,29 +63,32 @@ var NodeWebkitBrowser = function(baseBrowserDecorator, args,config) {
     var PACKAGE_JSON = path.join(STATIC_PATH, 'package.json');
 
     async.auto({
-      'directory': function(callback) {
-        ncp(SOURCE_PATH, STATIC_PATH,callback);
+      'command': function(callback) {
+        self._findCommand(callback);
       },
-      'copy':function(callback){
+      'directory': function(callback) {
+        ncp(SOURCE_PATH, STATIC_PATH, callback);
+      },
+      'copy': function(callback) {
         var items = config.copy.items;
         var len = config.copy.items.length;
         var base = config.copy.base;
         for( var i=0;i<config.copy.items.length;i++){
-          copy(base+items[i]+'/**/*',STATIC_PATH+items[i],function(){
+          copy(base+items[i]+'/**/*', STATIC_PATH+items[i], function(){
             if( --len == 0 ){
               callback();
             }
           })
         }
       },
-      'index.html:read': ['directory','copy', function(callback) {
+      'index.html:read': ['directory', 'copy', function(callback) {
         fs.readFile(INDEX_HTML, callback);
       }],
       'index.html:write': ['index.html:read', function(callback, results) {
         var content = results['index.html:read'].toString().replace('%URL%', url);
         fs.writeFile(INDEX_HTML, content, callback);
       }],
-      'package.json:read': ['directory','copy', function(callback) {
+      'package.json:read': ['directory', 'copy', function(callback) {
         fs.readFile(PACKAGE_JSON, callback);
       }],
       'package.json:write': ['package.json:read', function(callback, results) {
@@ -75,9 +96,9 @@ var NodeWebkitBrowser = function(baseBrowserDecorator, args,config) {
         options = merge(true, options, customOptions);
         fs.writeFile(PACKAGE_JSON, JSON.stringify(options), callback);
       }],
-      'exec': ['index.html:write', 'package.json:write', function(callback) {
+      'exec': ['index.html:write', 'package.json:write', 'command', function(callback, results) {
         process.env.NODE_PATH = searchPaths.join(path.delimiter);
-        self._execCommand(self._getCommand(), [STATIC_PATH]);
+        self._execCommand(results['command'], [STATIC_PATH]);
       }]
     });
   };
@@ -87,15 +108,15 @@ NodeWebkitBrowser.prototype = {
   name: 'NW.js',
 
   DEFAULT_CMD: {
-    linux: findpath(),
-    darwin: findpath(),
-    win32: findpath()
+    linux: null,
+    darwin: null,
+    win32: null
   },
 
   ENV_CMD: 'NODEWEBKIT_BIN'
 };
 
-NodeWebkitBrowser.$inject = ['baseBrowserDecorator', 'args','config.NWJSConfig'];
+NodeWebkitBrowser.$inject = ['baseBrowserDecorator', 'args', 'config.NWJSConfig'];
 
 // PUBLISH DI MODULE
 module.exports = {
