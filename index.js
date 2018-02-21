de0cef0b9fe616aec7ec50fd53465f719d39acab
Kieran Simpson <kierans777@gmail.com>
2018-02-21 18:31:56 +1100

    Fix formatting.

--- index.js
+++ index.js
@@ -97,9 +97,9 @@ var NodeWebkitBrowser = function(baseBrowserDecorator, args, config) {
       }],
       'exec': ['index.html:write', 'package.json:write', 'command', function(callback, results) {
         process.env.NODE_PATH = searchPaths.join(path.delimiter);
-				args.flags = args.flags || [];
+        args.flags = args.flags || [];
 
-				self._execCommand(results['command'], [STATIC_PATH].concat(args.flags));
+        self._execCommand(results['command'], [STATIC_PATH].concat(args.flags));
       }]
     });
   };
